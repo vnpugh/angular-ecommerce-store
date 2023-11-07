e839b8b9e5b7ccb76d7af4c81a897dcce1c5601a
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-11-06 21:54:37 -0600

    Updating controllers component

--- ecommerce/src/app/components/controllers/controllers.component.ts
+++ ecommerce/src/app/components/controllers/controllers.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { MockProductService } from 'src/app/services/mock-product.service';
 
 @Component({
   selector: 'app-controllers',
   templateUrl: './controllers.component.html',
   styleUrls: ['./controllers.component.css']
 })
-export class ControllersComponent {
+export class ControllersComponent implements OnInit {
 
+
+  products: Product[] = []; 
+
+  constructor(private productService: MockProductService) {}
+
+  ngOnInit() {
+    // Fetch all products from mockproducts
+    const allProducts = this.productService.getProducts();
+
+    // Filter to only include controllers
+    this.products = allProducts.filter(product => product.category.name === 'controllers');
+  }
 }
