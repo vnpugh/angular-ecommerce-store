22bb0ba138801b3a39c8cc4567cc7ee8a7058852
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-11-06 21:56:41 -0600

    Updating console component

--- ecommerce/src/app/components/game-consoles/game-consoles.component.ts
+++ ecommerce/src/app/components/game-consoles/game-consoles.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { MockProductService } from 'src/app/services/mock-product.service';
 
 @Component({
   selector: 'app-game-consoles',
   templateUrl: './game-consoles.component.html',
   styleUrls: ['./game-consoles.component.css']
 })
-export class GameConsolesComponent {
+export class GameConsolesComponent implements OnInit {
+
+  products: Product[] = []; 
+
+  constructor(private productService: MockProductService) {}
+
+  ngOnInit() {
+    // Fetch all products from mockproducts
+    const allProducts = this.productService.getProducts();
+
+    // Filter to only include consoles
+    this.products = allProducts.filter(product => product.category.name === 'consoles');
+  }
+
+
+
+
+
+
+
 
 }
