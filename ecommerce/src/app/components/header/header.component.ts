411c51c355a6577c9e77695bee23e62b2e90156c
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-11-09 00:08:37 -0600

    Search bar functionality

--- ecommerce/src/app/components/header/header.component.ts
+++ ecommerce/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 
 import { Component } from '@angular/core';
+import { MockProductService } from 'src/app/services/mock-product.service';
 
 
 @Component({
@@ -9,15 +10,29 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  searchQuery: string = '';
+  products: any[] = [];
+
+  constructor(private productService: MockProductService) {}
+
+  searchProducts() {
+    if (this.searchQuery.trim() !== '') { // Checking if there is non-empty text
+      this.products = this.productService.getProducts().filter(product => {
   
+        return product.name.toLowerCase().includes(this.searchQuery.toLowerCase());
 
-  isShopDropdownOpen: boolean = false;
 
-  toggleShopDropdown() {
-    this.isShopDropdownOpen = !this.isShopDropdownOpen;
- 
+    });
   }
-
 }
 
+  
 
+  // isShopDropdownOpen: boolean = false;
+
+  // toggleShopDropdown() {
+  //   this.isShopDropdownOpen = !this.isShopDropdownOpen;
+ 
+
+
+    }
